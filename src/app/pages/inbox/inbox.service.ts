2dbf03ad2edc4a78cd0401a99f241386edff376b
jentapia <jentapia@gmail.com>
2021-08-30 01:52:03 +0100

    Update inbox.service.ts

--- src/app/pages/inbox/inbox.service.ts
+++ src/app/pages/inbox/inbox.service.ts
@@ -14,61 +14,61 @@ export class InboxService {
     {
       id: 1,
       email: "email1@email.com",
-      subject: "first subject",
+      subject: "First subject",
       message: "This is a fake first email. Thanks"
     },
     {
       id: 2,
       email: "email2@email.com",
-      subject: "second subject",
+      subject: "Second subject",
       message: "This is a fake second email. Thanks"
     },
     {
       id: 3,
       email: "email3@email.com",
-      subject: "third subject",
+      subject: "Third subject",
       message: "This is a fake third email. Thanks"
     },
     {
       id: 4,
       email: "email4@email.com",
-      subject: "fourth subject",
+      subject: "Fourth subject",
       message: "This is a fake fourth email. Thanks"
     },
     {
       id: 5,
       email: "email5@email.com",
-      subject: "fifth subject",
+      subject: "Fifth subject",
       message: "This is a fake fifth email. Thanks"
     },
     {
       id: 6,
       email: "email6@email.com",
-      subject: "sixth subject",
+      subject: "Sixth subject",
       message: "This is a fake sixth email. Thanks"
     },
     {
       id: 7,
       email: "email7@email.com",
-      subject: "seventh subject",
+      subject: "Seventh subject",
       message: "This is a fake seventh email. Thanks"
     },
     {
       id: 8,
       email: "email8@email.com",
-      subject: "eighth subject",
+      subject: "Eighth subject",
       message: "This is a fake eighth email. Thanks"
     },
     {
       id: 9,
       email: "email9@email.com",
-      subject: "nineth subject",
+      subject: "Nineth subject",
       message: "This is a fake nineth email. Thanks"
     },
     {
       id: 10,
       email: "email10@email.com",
-      subject: "tenth subject",
+      subject: "Tenth subject",
       message: "This is a fake tenth email. Thanks"
     }
   ]
